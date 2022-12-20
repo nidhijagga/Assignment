b63cd5006a7995ca629032eb7726aac3db0fe963
nidhijagga <nidhijagga5239@gmail.com>
2022-12-20 17:20:20 +0530

    Task 9 Filter

--- dom.js
+++ dom.js
@@ -182,6 +182,8 @@
 
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let itemDescription = document.getElementById('description');
+let filter = document.getElementById('filter');
 
 //Function addItem
 
@@ -190,8 +192,10 @@ const addItem = (e) => {
     
     //Get Input Value
     let newItem = document.getElementById('item').value;
-    console.log(newItem);
-    
+    // console.log(newItem);
+    //Get the input valu of Description
+    let itemDes = document.getElementById('description').value;
+
     //Create new Element
     let li = document.createElement('li');
 
@@ -201,6 +205,11 @@ const addItem = (e) => {
     //Add text node with input value
     li.appendChild(document.createTextNode(newItem));
     itemList.appendChild(li);
+
+    //Add text node with the item Description Value
+    li.appendChild(document.createElement('br'));
+     li.appendChild(document.createTextNode( itemDes));
+     itemDescription.appendChild(li);
 
     //Create Delete button
     let delButton = document.createElement('button');
@@ -221,7 +230,7 @@ const addItem = (e) => {
     let editButton = document.createElement('button');
     
     //Add Classes to edit Button
-    editButton.className = 'btn btn-secondary float-right btn-sm col-2 mx-2';
+    editButton.className = 'btn btn-secondary float-right btn-sm mx-3 px-3';
 
     //Append Text Node
     editButton.appendChild(document.createTextNode('Edit'));
@@ -241,11 +250,41 @@ const removeItem = (e) => {
   }
 }
 
+
+//Filter Items
+const filterItems =(e) =>{
+
+  //Convert to lower Case
+  let text = e.target.value.toLowerCase();
+  
+  //get list
+  let items = itemList.getElementsByTagName('li');
+  // console.log(items);
+  //Convert to array
+  let arr = Array.from(items);
+  console.log(arr);
+  arr.filter((item) =>{
+    let itemName = item.firstChild.textContent;
+    let itemDescription = item.firstChild.nextSibling.nextSibling.textContent;
+    // console.log(itemDescription);
+    if(itemName.toLowerCase().indexOf(text)!=-1 || itemDescription.toLowerCase().indexOf(text)!=-1){
+      item.style.display = 'block';
+    }
+    else{
+      item.style.display = 'none';
+    }
+  })
+}
+
+
 //Form submit event
 form.addEventListener('submit', addItem);
 
 //Delete Event
 itemList.addEventListener('click' , removeItem);
+
+//filter Event
+filter.addEventListener('keyup', filterItems)
 
 
 
